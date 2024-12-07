7eb351f55a281c6610fd976b1312853ac8f4292e
Jason <quietjason@gmail.com>
2024-12-07 15:36:53 +0800

    Handle focus.

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,9 +1,22 @@
-import { useState } from 'react'
+import { useState, useRef, useEffect } from 'react'
+
+function usePrevious(value) {
+  const ref = useRef()
+  useEffect(() => {
+    ref.current = value
+  })
+  return ref.current
+}
 
 function Todo({ id, name, completed, toggleTaskCompleted, deleteTask, editTask }) {
 
   const [isEditing, setIsEditing] = useState(false)
   const [newName, setNewName] = useState('')
+  
+  const wasEditing = usePrevious(isEditing)
+
+  const editFieldRef = useRef(null)
+  const editButtonRef = useRef(null)
 
   function handleEdit(e) {
     e.preventDefault()
@@ -18,7 +31,14 @@ function Todo({ id, name, completed, toggleTaskCompleted, deleteTask, editTask }
         <label className="todo-label" htmlFor={id}>
           New name for {name}
         </label>
-        <input id={id} className="todo-text" type="text" value={newName} onChange={e => setNewName(e.target.value)} />
+        <input 
+          id={id} 
+          ref={editFieldRef} 
+          className="todo-text" 
+          type="text" 
+          value={newName} 
+          onChange={e => setNewName(e.target.value)} 
+        />
       </div>
       <div className="btn-group">
         <button type="button" className="btn todo-cancel" onClick={() => setIsEditing(false)}>
@@ -47,7 +67,7 @@ function Todo({ id, name, completed, toggleTaskCompleted, deleteTask, editTask }
         </label>
       </div>
       <div className="btn-group">
-        <button type="button" className="btn" onClick={() => setIsEditing(true)}>
+        <button ref={editButtonRef} type="button" className="btn" onClick={() => setIsEditing(true)}>
           Edit <span className="visually-hidden">{name}</span>
         </button>
         <button
@@ -60,6 +80,14 @@ function Todo({ id, name, completed, toggleTaskCompleted, deleteTask, editTask }
     </div>
   );  
 
+  useEffect(() => {
+    if (!wasEditing && isEditing) {
+      editFieldRef.current.focus()
+    } else if (wasEditing && !isEditing) {
+      editButtonRef.current.focus()
+    }
+  }, [wasEditing, isEditing]);
+  
   return (isEditing ? editingTemplate : viewTemplate)
 }
 
